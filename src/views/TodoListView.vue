441facbd05981d174c1ab4eae336fe97146b227d
clarencelinwork <clarencelinwork@gmail.com>
2025-09-06 14:30:05 +0800

    [chore] remove unuse function and variable

--- src/views/TodoListView.vue
+++ src/views/TodoListView.vue
@@ -142,18 +142,6 @@ function checkItem(id) {
     })
 }
 
-function _uuid() {
-  let d = Date.now()
-  if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
-    d += performance.now() //use high-precision timer if available
-  }
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-    const r = (d + Math.random() * 16) % 16 | 0
-    d = Math.floor(d / 16)
-    return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16)
-  })
-}
-
 function checkLogin() {
   const token = Cookies.get('token')
   if (token !== undefined) {
@@ -221,7 +209,7 @@ function signOutButton() {
         },
       }
     )
-    .then((response) => {
+    .then(() => {
       Cookies.remove('token')
       Cookies.remove('tokenExpired')
       router.push({name: 'sign-in'});
